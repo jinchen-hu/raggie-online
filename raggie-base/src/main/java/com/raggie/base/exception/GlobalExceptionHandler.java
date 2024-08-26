a099c806bf1191679e158ab39feb8fd49ce6cef5
Luke Hu <jinchenhu.luke@gmail.com>
2024-08-26 01:40:02 -0700

    add validators

--- raggie-base/src/main/java/com/raggie/base/exception/GlobalExceptionHandler.java
+++ raggie-base/src/main/java/com/raggie/base/exception/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package com.raggie.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @Slf4j
 @ControllerAdvice
@@ -24,5 +29,18 @@ public class GlobalExceptionHandler {
     public RestErrorResponse customException(final Exception e) {
         log.error("System error: {}", e.getMessage());
         return new RestErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse methodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        StringBuffer stringBuffer = new StringBuffer();
+        fieldErrors.forEach(fieldError -> stringBuffer.append(fieldError.getDefaultMessage()).append(","));
+        log.error(stringBuffer.toString());
+        return new RestErrorResponse(stringBuffer.toString());
     }
 }
--- raggie-content/raggie-content-api/src/main/java/com/raggie/content/api/CourseBaseInfoController.java
+++ raggie-content/raggie-content-api/src/main/java/com/raggie/content/api/CourseBaseInfoController.java
@@ -11,6 +11,7 @@ import com.raggie.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class CourseBaseInfoController {
 
     @ApiOperation("")
     @PostMapping("/course")
-    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto){
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated AddCourseDto addCourseDto){
         //获取到用户所属机构的id
         Long companyId = 1232141425L;
 //        int i = 1/0;
